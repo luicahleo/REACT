c638ed0509f83bdcf3565a5f1cd2e4e5512c8312
luis <luisrolandocahuanaleon@gmail.com>
2023-02-13 09:34:58 +0100

    mostrando componente con un &&

--- 04-gif-expert-app2/src/components/GifGrid.jsx
+++ 04-gif-expert-app2/src/components/GifGrid.jsx
@@ -4,12 +4,13 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
-
-  const { images, isLoading } = useFetchGifs( category );
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
-      <div className="card-grid"> 
+      <div className="card-grid">
+        {isLoading && <h3>Cargando...</h3>}
+
         {images.map((images) => (
           <GifItem key={images.id} {...images} />
         ))}
